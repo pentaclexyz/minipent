d5006f957054d4c2ff28363b7e81e6ddf46743dd
pentcle <pentacle@pentacle.xyz>
2022-08-20 16:52:22 +0100

    feat: add project page intro

---
+++ src/api/projectpage/controllers/projectpage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  projectpage controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::projectpage.projectpage');
---
+++ src/api/projectpage/routes/projectpage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * projectpage router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::projectpage.projectpage');
