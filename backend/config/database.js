24d297b007bafb77db157b2ebfc4a839da0806b1
pentcle <pentacle@pentacle.xyz>
2022-08-21 18:04:36 +0100

    fix: old package.json not for railway

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'minipent'),
+      host: env('HOST', '127.0.0.1'),
+      port: env.int('PORT', 5432),
+      database: env('DATABASE', 'minipent'),
       ssl: env.bool(true),
     },
   },
