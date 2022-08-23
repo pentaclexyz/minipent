472ed8571054710f28b0605ef92c8f8a4aedb015
pentcle <pentacle@pentacle.xyz>
2022-08-23 21:24:20 +0100

    feat: add ppl

--- frontend/components/personCardMini.js
+++ frontend/components/personCardMini.js
@@ -1,13 +1,12 @@
+import {getStrapiMedia} from "../lib/media";
+
 export function PersonCardMini({person}) {
-    return (
-        <article className={"col-span-4"}>
-            <div className="flex flex-col px-6 pt-6 pb-3 rounded-2xl bg-white/5 cursor-pointer hover:bg-white/10">
-                <div className="flex flex-col gap-3 mt-auto">
-                    <div className={"text-sm"}>{person.name}</div>
-                    <div className={"text-sm"}>{person.bio}</div>
-                    <div className={"text-sm"}>@{person.twitter}</div>
-                </div>
-            </div>
-        </article>
-    );
+    return (<div>
+        <a href={`https://www.twitter.com/${person.twitter}`}
+           rel="nofollow noreferrer noopener" target={"_blank"}
+           className="flex flex-col p-6 mb-6 rounded-2xl bg-white/5 cursor-pointer hover:bg-white/10 align-baseline">
+            <img className={"rounded-full w-16 mb-6"} src={getStrapiMedia(person.avatar)}/>
+            <div>@{person.twitter}</div>
+        </a>
+    </div>);
 }
--- frontend/components/planningCard.js
+++ frontend/components/planningCard.js
@@ -4,15 +4,14 @@ import {PersonCardMini} from "./personCardMini";
 export default function PlanningCard({planning}) {
     return (
         <section className={"rounded-2xl bg-white/10 p-8"}>
-            <h1>{planning.name}</h1>
-
             <article className={"grid grid-cols-12 gap-x-8 whitespace-pre-wrap"}>
-                <article className="col-span-9">
+                <article className="col-span-10">
+                    <h1>{planning.name}</h1>
                     <ReactMarkdown>{planning.description}</ReactMarkdown>
                     <ReactMarkdown>{planning.content}</ReactMarkdown>
                 </article>
-                <article className="col-span-3">
-                    <h2>who</h2>
+                <article className="col-span-2">
+                    <h2 className={"pt-0"}>Who</h2>
                     {planning.people.data.map((person, i) => (
                         <PersonCardMini person={person.attributes} key={i} />
                     ))}
