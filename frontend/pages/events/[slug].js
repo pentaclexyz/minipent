611e625bfaea0982322bb9005bf02a91b20d74f8
pentcle <pentacle@pentacle.xyz>
2022-09-07 14:49:52 +0100

    fix: events

--- frontend/pages/events/[slug].js
+++ frontend/pages/events/[slug].js
@@ -1,18 +1,30 @@
-import {fetchAPI, getSlugsForPath} from "../../lib/api";
+import {fetchAPI} from "../../lib/api";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
-import EventCard from "../../components/eventCard";
+import ReactMarkdown from "react-markdown";
 
 
 const Event = ({event}) => {
     const seo = {
-        metaTitle: event.attributes.name,
+        metaTitle: event?.attributes?.name,
+        metaDescription: `Details for event ${event?.attributes?.name}`,
     };
 
     return (
         <Layout>
             <Seo seo={seo}/>
-            <EventCard event={event.attributes} id={event.id}/>
+            <section>
+                <h1>{event?.attributes?.name}</h1>
+                <article className={"pb-6 editorial"}>
+                    <ReactMarkdown>{event?.attributes?.details}</ReactMarkdown>
+                </article>
+                <article className={"pb-6 editorial"}>
+                    <ReactMarkdown>{event?.attributes?.planning}</ReactMarkdown>
+                </article>
+            </section>
+            <aside>
+                <p></p>
+            </aside>
         </Layout>
     );
 };
@@ -21,24 +33,29 @@ export default Event;
 
 export async function getStaticPaths() {
     return {
-        paths: await getSlugsForPath("events"),
+        // paths: await getSlugsForPath("events"),
+        paths: [],
         fallback: true,
     };
 }
 
 export async function getStaticProps({params}) {
-    const events = await fetchAPI("/events", {
-        filters: {slug: params.slug},
-        populate: {
-            events: {populate: "*"},
-            people: {populate: "*"},
-        },
-    });
-
-    return {
-        props: {
-            event: events.data[0],
-        },
-        revalidate: 1,
-    };
+    try {
+        const matchingEvents = await fetchAPI("/events", {
+            filters: {slug: params.slug},
+        });
+        return {
+            props: {
+                event: matchingEvents.data[0],
+            },
+            revalidate: 1,
+        };
+    } catch (e) {
+        return {
+            props: {
+                event: {},
+            },
+            revalidate: 1,
+        };
+    }
 }
