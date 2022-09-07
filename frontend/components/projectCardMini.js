520e0bd042cb4dcdccb01569c824ead5b4f177a8
pentcle <pentacle@pentacle.xyz>
2022-09-07 11:17:32 +0100

    feat: add intro to project page

--- frontend/components/projectCardMini.js
+++ frontend/components/projectCardMini.js
@@ -9,14 +9,12 @@ function removeTrailingSlash(str = '') {
 export function ProjectCardMini({item}) {
 
     return (
-        <div
+        <article
             className="flex flex-col justify-between col-span-4 gap-2 px-6 pt-6 pb-6 rounded-2xl bg-white/5 cursor-pointer hover:bg-white/10">
 
             <Link href={{pathname: `/projects/${item.slug}`}}>
                 <h2 className={"cursor-pointer text-color-secondary-700 fave-link py-0"}>{item.name}</h2>
             </Link>
-
-            <div>{item.coverImage}</div>
             <div className={"text-sm line-clamp-3"}>{item.description}</div>
             <div>
                 <a href={item.url} rel="nofollow noreferrer noopener" target={"_blank"}
@@ -30,10 +28,10 @@ export function ProjectCardMini({item}) {
                        target={"_blank"}
                        className={"line-clamp-2"}>
                         <p className={"text-sm cursor-pointer hover:text-color-secondary-700 line-clamp-1 external-link"}>
-                            @{item.twitterId}
+                            {removeTrailingSlash(item.twitterId?.split("//")[1])}
                         </p>
                     </a>)}
             </div>
-        </div>
+        </article>
     );
 }
--- frontend/pages/projects/index.js
+++ frontend/pages/projects/index.js
@@ -1,13 +1,27 @@
 import Layout from "../../components/layout";
 import {fetchAPI} from "../../lib/api";
 import {ProjectCardMini} from "../../components/projectCardMini";
+import ReactMarkdown from "react-markdown";
+import {getStrapiMedia} from "../../lib/media";
 
-export default function ProjectOverview({projects}) {
+export default function ProjectOverview({intros, projects}) {
   return (
     <Layout>
       <h1>Projects</h1>
+        {intros.map((intro, i) => (
+            <article className={"grid mt-2 md:mt-6 lg:mt-10 md:gap-x-10 md:grid-cols-2 lg:gap-x-20"} key={i}>
+                <div className={"col-span-1 mb-10"}>
+                    <h1 className={"text-4xl"}>{intro.title}</h1>
+                    <ReactMarkdown>{intro.content}</ReactMarkdown>
+                </div>
+                <div className={"col-span-1"}>
+                    <img className={"rounded-full"} src={getStrapiMedia(intro.image)}/>
+                </div>
+            </article>
+        ))}
       <div className="grid gap-4 grid-cols-4 md:grid-cols-8 lg:grid-cols-12">
-        {projects.map((project, i) => (
+        {projects.data.map((project, i) => (
+            // <p key={i}>{project.attributes.name}</p>
             <ProjectCardMini item={project.attributes} key={i} />
         ))}
       </div>
@@ -17,17 +31,17 @@ export default function ProjectOverview({projects}) {
 
 export async function getStaticProps() {
 
-    const projects = await fetchAPI("/projectpage", {
+    const projectpage = await fetchAPI("/projectpage", {
         populate: {
-            projects: {populate: "*"},
             intros: {populate: "*"},
+            projects: {populate: "*"},
         },
     });
 
   return {
     props: {
-      projects: projects.data,
-      intros: projects.data,
+        intros: projectpage.data.attributes.intros,
+        projects: projectpage.data.attributes.projects,
     },
     revalidate: 1,
   };
