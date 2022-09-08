fdcea75da51939731a552754c75c756dc4f18ba0
pentcle <pentacle@pentacle.xyz>
2022-09-08 14:01:59 +0100

    feat: add ppl to planning items

--- frontend/components/tasksCard.js
+++ frontend/components/tasksCard.js
@@ -10,17 +10,17 @@ export function TasksCard({tasks}) {
             </colgroup>
             <thead>
             <tr className={""}>
-                <th className={"border-collapse border border-white p-2"}>Name</th>
-                <th className={"border-collapse border border-white p-2"}>Status</th>
-                <th className={"border-collapse border border-white p-2"}>Details</th>
+                <th className={"border-collapse border border-white"}>Name</th>
+                <th className={"border-collapse border border-white"}>Status</th>
+                <th className={"border-collapse border border-white"}>Details</th>
             </tr>
             </thead>
             <tbody>
             {tasks.map((task, i) => (
                 <tr key={i}>
-                    <td className={"border-collapse border border-white p-2 w-1/4"}><ReactMarkdown>{task.name}</ReactMarkdown></td>
-                    <td className={"border-collapse border border-white p-2 w-1/2"}><ReactMarkdown>{task.status}</ReactMarkdown></td>
-                    <td className={"border-collapse border border-white p-2 w-1/4"}><ReactMarkdown>{task.content}</ReactMarkdown></td>
+                    <td className={"border-collapse border border-white w-1/4"}><ReactMarkdown>{task.name}</ReactMarkdown></td>
+                    <td className={"border-collapse border border-white w-1/2"}><ReactMarkdown>{task.status}</ReactMarkdown></td>
+                    <td className={"border-collapse border border-white w-1/4"}><ReactMarkdown>{task.content}</ReactMarkdown></td>
                 </tr>
             ))}
             </tbody>
