5981c479fb2f1e885e0b3ca79098e97c13f0540b
pentcle <pentacle@pentacle.xyz>
2022-09-08 17:04:40 +0100

    feat: labelling

--- frontend/components/tasksCard.js
+++ frontend/components/tasksCard.js
@@ -6,26 +6,24 @@ export function TasksCard({tasks}) {
         <table className={"table-fixed text-sm my-4 p-2 w-full text-left align-text-top editorial"}>
             <colgroup>
                 <col className={"w-1/4"}/>
-                <col className={"w-1/4"}/>
+                <col className={"w-3/4"}/>
                 <col/>
             </colgroup>
             <thead>
             <tr className={""}>
                 <th className={"border-collapse border border-white"}>Name</th>
-                <th className={"border-collapse border border-white"}>Status</th>
                 <th className={"border-collapse border border-white"}>Details</th>
             </tr>
             </thead>
             <tbody>
             {tasks.map((task, i) => (
                 <tr key={i}>
-                    <td className={"border-collapse border border-white w-1/4"}>
+                    <td className={"border-collapse border border-white"}>
                         <ReactMarkdown>{task.name}</ReactMarkdown>
-                        <div className={"mt-2"}><Label label={task.status} key={i} bgColor={task.status}
-                                                   txtColor={"text-primary"}/></div>
+                        <Label label={task.status} key={i} bgColor={task.status}
+                                                   txtColor={"text-secondary"}/>
                     </td>
-                    <td className={"border-collapse border border-white w-1/2"}></td>
-                    <td className={"border-collapse border border-white w-1/4"}>
+                    <td className={"border-collapse border border-white"}>
                         <ReactMarkdown>{task.content}</ReactMarkdown></td>
                 </tr>
             ))}
