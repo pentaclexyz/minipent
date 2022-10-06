583c8f03ab963be5130a7dac4692289260ce223d
pentcle <pentacle@pentacle.xyz>
2022-10-06 16:27:04 +0100

    feat: local uploads

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -6,21 +6,21 @@ module.exports = ({ env }) => ({
     },
     upload: {
       config: {
-        // providerOptions: {
-        //   localServer: {
-        //     maxage: 300000
-        //   },
-        // },
-        provider: 'cloudinary',
         providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+          localServer: {
+            maxage: 300000
+          },
         },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+        // provider: 'cloudinary',
+        // providerOptions: {
+        //   cloud_name: env('CLOUDINARY_NAME'),
+        //   api_key: env('CLOUDINARY_KEY'),
+        //   api_secret: env('CLOUDINARY_SECRET'),
+        // },
+        // actionOptions: {
+        //   upload: {},
+        //   delete: {},
+        // },
       },
     },
   });
