8f21f2038af846455c91cfddd0fe8551a3bd3abc
pentcle <pentacle@pentacle.xyz>
2022-10-06 16:49:19 +0100

    feat: local uploads

--- frontend/lib/media.js
+++ frontend/lib/media.js
@@ -1,7 +1,7 @@
-import {getStrapiImageURL} from "./api";
+import {getStrapiURL} from "./api";
 
 export function getStrapiMedia(media) {
   const { url } = media?.data?.attributes || {};
-  const imageUrl = url?.startsWith("/") ? getStrapiImageURL(url) : url;
+  const imageUrl = url?.startsWith("/") ? getStrapiURL(url) : url;
   return imageUrl;
 }
