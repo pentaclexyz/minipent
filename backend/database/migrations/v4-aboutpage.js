85d8e4833ead1e2d16efa14c44cadf6a121bf1ee
pentacle <pentacle@pentacle.xyz>
2022-10-25 23:22:05 +0100

    fix: add if statement to create new about table (#46)

--- backend/database/migrations/v4-aboutpage.js
+++ backend/database/migrations/v4-aboutpage.js
@@ -1,8 +1,12 @@
 module.exports = {
   async up(knex) {
-    await knex.schema.createTable('abouts', function (table) {
-      table.increments();
-      table.timestamps();
-    })
+    await knex.schema.hasTable('abouts').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('abouts', function(table) {
+            table.increments();
+            table.timestamps();
+        });
+      }
+    });
   },
 };
