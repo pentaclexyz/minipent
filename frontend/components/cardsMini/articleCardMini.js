9cbe428c239ed05a5d39e81e17c937bf46caf607
pentcle <pentacle@pentacle.xyz>
2022-11-15 10:40:19 +0000

    feat: canto project refining

--- frontend/components/cardsMini/articleCardMini.js
+++ frontend/components/cardsMini/articleCardMini.js
@@ -3,34 +3,41 @@ import Link from "next/link";
 import {PersonCardTiny} from "./personCardTiny";
 import {getStrapiMedia} from "../../lib/media";
 import Image from "next/image";
+import StyledLink from "../nav/styledLink";
 
 export function ArticleCardMini({item, section}) {
+
+    function removeTrailingSlash(str = "") {
+        return str.replace(/\/+$/, "");
+    }
+
+    const coverImage = item.coverImage
 
     return (
         <article className={"col-span-3"}>
             <div className="p-card-inner">
 
                 <div className="flex flex-col gap-3">
+
                     <Link href={{pathname: `/${section}/${item.slug}`}}>
                         <div className={"cursor-pointer"}>
                             <h2 className={"txt-secondary internal-link"}>{item.name}</h2>
-                            {item.coverImage &&
-                                <div><Image src={getStrapiMedia(item.coverImage)} className={"object-cover"} width={300} height={200}/></div>
-                            }
                         </div>
                     </Link>
-
-                    <div className={"text-sm"}>{item.description}</div>
-                    {item.date &&
-                        <div className={"text-sm"}>{item.date}</div>
+                    <StyledLink url={item.url} text={item.url}/>
+                    {coverImage &&
+                        <div><Image src={getStrapiMedia(item.coverImage)} className={"object-cover"} width={300}
+                                    height={200}/></div>
                     }
+                    {item.description && <div className={"text-sm"}>{item.description}</div>}
+                    {item.date && <div className={"text-sm"}>{item.date}</div>}
                     {item.people && <article className="flex gap-x-6">
                         {item.people.data.map((person, i) => (
                             <PersonCardTiny person={person.attributes} key={i}/>
                         ))}
                     </article>}
-                </div>
 
+                </div>
             </div>
         </article>
     );
--- frontend/components/nav/styledLink.js
+++ frontend/components/nav/styledLink.js
@@ -1,14 +1,17 @@
-const StyledLink = (props) => {
+function removeTrailingSlash(str = "") {
+  return str.replace(/\/+$/, "");
+}
+
+export default function StyledLink(props) {
   return (
-    <>
-      {props.url && (
-        <div className="text-sm pb-2">
-          <a href={`${props.url}`} target={"_blank"} rel="noopener noreferrer" className="hover:txt-primary external-link">
-            {props.text}
-          </a>
-        </div>
-      )}
-    </>
+      <>
+        {props.url && (
+            <a href={`${props.url}`} target={"_blank"} rel="noopener noreferrer"
+               className="flex items-center tracking-wide px-2 py-0.5 -ml-2 mr-3 hover:underline">
+              <span>{props.icon && (<img src={`${props.icon}`} alt={`${props.icon}`} height={24} width={24} className={"mr-2 bg-bg-card-secondary rounded-2xl border-4 border-bg-card-secondary"}/>)}</span>
+              <span>{removeTrailingSlash(props.text?.split("//")[1])}</span>
+            </a>
+        )}
+      </>
   );
 };
-export default StyledLink;
