f078e61b0640e077945e49b450c825338ec9d65d
pentcle <pentacle@pentacle.xyz>
2022-11-19 13:20:07 +0000

    feat: add projects to events

--- frontend/pages/events/[slug].js
+++ frontend/pages/events/[slug].js
@@ -8,6 +8,7 @@ import React from "react";
 import {getStrapiMedia} from "../../lib/media";
 import {ArticleCardMini} from "../../components/cardsMini/articleCardMini";
 import CardContainerLayout from "../../components/layouts/cardContainerLayout";
+import StyledLink from "../../components/nav/styledLink";
 
 
 const Event = ({event}) => {
@@ -15,32 +16,37 @@ const Event = ({event}) => {
         metaTitle: event?.attributes?.name,
     };
 
+    const web = "https://pentacle.xyz/images/icons/web.svg"
+
     return (
         <Layout>
             <Seo seo={seo}/>
             <LayoutPageContent>
                 <section className={"md:col-span-8"}>
                     <h1>{event.attributes.name}</h1>
-                    {/*{event?.attributes?.projects.data.map((item, i) => (*/}
-                    {/*    <p key={i}>{item.attributes.name}</p>*/}
-                    {/*))}*/}
+
+                    <div className={"py-4"}><StyledLink url={event.attributes.url} text={event.attributes.url} icon={web}/></div>
+
+                    <article className={"pb-6 editorial"}>
+                        <ReactMarkdown>{event?.attributes?.details}</ReactMarkdown>
+                    </article>
+
                     <CardContainerLayout>
                     {event.attributes.projects.data.map((project, i) => (
                         <ArticleCardMini item={project.attributes} section={"projects"} key={i}/>
                     ))}
                     </CardContainerLayout>
-                    <article className={"pb-6 editorial"}>
-                        <ReactMarkdown>{event?.attributes?.details}</ReactMarkdown>
-                    </article>
 
 
                     <article className={"pb-6 editorial"}>
                         <ReactMarkdown>{event?.attributes?.planning}</ReactMarkdown>
                     </article>
                 </section>
-                {/*<article className="pt-6 md:pt-0 md:col-span-4 sm:mt-4">*/}
-                {/*    <ArticleAside item={event.attributes}/>*/}
-                {/*</article>*/}
+
+                <article className="pt-6 md:pt-0 md:col-span-4 sm:mt-4">
+                    <ArticleAside item={event.attributes}/>
+                </article>
+
             </LayoutPageContent>
         </Layout>
     );
