4e3acedb7f4b9411a9a647c6bbce7c966d1f0e1f
pentcle <pentacle@pentacle.xyz>
2022-11-19 23:12:31 +0000

    fun: default to canto theme

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -2,7 +2,7 @@ import {Html, Head, Main, NextScript} from "next/document";
 
 export default function Document() {
     return (
-        <Html data-theme={"light"}>
+        <Html data-theme={"canto"}>
             <Head></Head>
             <body className={"bg-p-bg text-txt-primary"}>
                 <Main/>
