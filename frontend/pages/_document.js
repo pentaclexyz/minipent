593522c54d997a801263ae5ace1b963d952a9028
pentcle <pentacle@pentacle.xyz>
2022-11-20 09:39:10 +0000

    fix: ios zoom

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,7 +3,9 @@ import {Html, Head, Main, NextScript} from "next/document";
 export default function Document() {
     return (
         <Html data-theme={"canto"}>
-            <Head></Head>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            </Head>
             <body className={"bg-p-bg text-txt-primary"}>
                 <Main/>
                 <NextScript/>
