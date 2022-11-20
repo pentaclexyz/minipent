98d5a981bac84408dd379e1ac45fed7ebbf41951
pentcle <pentacle@pentacle.xyz>
2022-11-20 15:38:00 +0000

    fix: events

--- frontend/pages/events/[slug].js
+++ frontend/pages/events/[slug].js
@@ -48,7 +48,7 @@ const Event = ({event}) => {
 
 
                 <article className="pt-6 md:pt-0 md:col-span-2 sm:mt-4">
-                    <ArticleAside item={event.attributes}/>
+                    {/*People*/}
                 </article>
 
             </LayoutPageContent>
